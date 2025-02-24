8ce18fba8cb41b188326157d352f349856016151
Ravena Carvalho <111706420+ravenacarvalho@users.noreply.github.com>
2025-02-24 09:23:57 -0300

    Update minesweeper.h

--- minesweeper.h
+++ minesweeper.h
@@ -1,12 +1,14 @@
 #ifndef MINESWEEPER_H
 #define MINESWEEPER_H
 
+// Constantes de configuração do tabuleiro
 #define MIN_LINHAS 5
 #define MAX_LINHAS 26
 #define MIN_COLUNAS 5
 #define MAX_COLUNAS 40
 #define MIN_MINAS 7
 
+// Estrutura que representa uma célula do tabuleiro
 typedef struct Celula {
     int mina;
     int vizinhos;
@@ -15,6 +17,7 @@ typedef struct Celula {
     struct Celula *cima, *baixo, *esquerda, *direita;
 } Celula;
 
+// Declaração das funções utilizadas no jogo
 Celula **criarTabuleiro(int linhas, int colunas);
 void posicionarMinas(Celula **tabuleiro, int linhas, int colunas, int num_minas);
 void calcularVizinhos(Celula **tabuleiro, int linhas, int colunas);
